44795f26ed87e85b45e0a2801b437a9bdb432c18
LeeSooYeon(수연) <74709068+ssooynn@users.noreply.github.com>
2021-03-02 16:28:41 +0900

    Update bigger_is_greater.py

--- bigger_is_greater.py
+++ bigger_is_greater.py
@@ -9,24 +9,20 @@ How to solve it
 
 def biggerIsGreater(w):
     w = list(w)
-    i = len(w) - 1
-    while i >= 0 and w[i-1] >= w[i]:
+    i, j = len(w) - 1, len(w) - 1
+    
+    while 0 <= i and w[i] <= w[i-1]:
         i -= 1
 
         if i <= 0:
             return "no answer"
         
-    j = len(w) - 1
     while w[j] <= w[i-1]:
         j -= 1
 
     w[i-1], w[j] = w[j], w[i-1]
 
-    j = len(w) - 1
-    while i < j :
-        w[i], w[j] = w[j], w[i]
-        i += 1
-        j -= 1
+    w = w[:i] + list(reversed(w[i:]))
 
     return ''.join(w)
     
